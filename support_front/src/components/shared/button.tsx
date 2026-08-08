5b0e11bb811ba68f8d6cb125e2c0409bd1e91c81
eberne <edmond.berne@inrae.fr>
2026-08-08 17:51:07 +0200

    style: download shdcn components

---
+++ support_front/src/components/shared/button.tsx
@@ -0,0 +1,43 @@
+import { Button } from "../ui/button";
+import { Spinner } from "../ui/spinner";
+
+
+type BtnVersion = "primary";
+
+interface BtnProps {
+    children: React.ReactNode,
+    version: BtnVersion,
+    onClick?: () => void,
+    disabled?: boolean,
+    isLoading?: boolean,
+}
+
+
+/**
+ * Wrapper Component to handle custom btn style
+ * @param props 
+ * @returns 
+ */
+export function Btn(props : BtnProps) {
+
+
+    const getBtnClass = () => {
+        switch (props.version) {
+            case "primary":
+                return "bg-white/20 hover:bg-white/30 cursor-pointer";
+        }
+    } 
+
+
+    return (
+        <Button 
+            className={getBtnClass()} 
+            type="submit" 
+            onClick={props.onClick}
+            disabled={Boolean(props.disabled) || Boolean(props.isLoading)} 
+        >   
+            {props.isLoading && <Spinner data-icon="inline-start" />}
+            {props.children}
+        </Button>
+    )
+}
